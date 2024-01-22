bd049b4f32cd12b7935d818ed2fc4197cb58f023
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2024-01-22 22:26:06 +0800

    新版UI调整、新增顶部介绍、使用UI-SDK设置选择小窗

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,6 +6,7 @@ import NumCharactersCheck from "@/checkType/NumCharactersCheck.vue";
 import NumRangeCheck from "@/checkType/NumRangeCheck.vue";
 import TrimCheck from "@/checkType/TrimCheck.vue";
 import { feildTypeToData, checkTypeValueFn } from "@/utils/fieldType.js";
+import HeaderIntro from "@/components/HeaderIntro.vue";
 
 export default {
   components: {
@@ -17,6 +18,7 @@ export default {
     NumCharactersCheck,
     NumRangeCheck,
     TrimCheck,
+    HeaderIntro,
   },
   setup() {
     const formData = ref({
@@ -29,8 +31,6 @@ export default {
           // 待添加属性
         },
       ],
-      startIndex: 0, // 开始索引
-      endIndex: 0, // 结束索引
     });
     // 展示数据
     const tableMetaList = ref([]);
@@ -44,6 +44,8 @@ export default {
     const loading = ref(false);
     const noPassTableData = ref([]); // 检查不通过数据表
     const NoPassDataFields = ref([]); // 记录每条record没有通过的字段数组
+
+    const selectRecordList = ref([]); // 小窗选中记录列表
 
     onMounted(async () => {
       loading.value = true;
@@ -62,68 +64,55 @@ export default {
       loading.value = false;
     });
 
-    // 获取选中table的所有记录
-    const getAllRecordList = async () => {
-      let startIndexValue = parseInt(formData.value.startIndex);
-      let endIndexValue = parseInt(formData.value.endIndex);
-      if (startIndexValue < 0 || endIndexValue < 1) {
-        alert("请输入起始索引和结束索引");
-        return [];
-      }
-      if (startIndexValue > endIndexValue) {
-        alert("起始索引不能大于结束索引");
-        return [];
-      }
+    // ************************ 事件 ****************************
+    // 改变table表
+    const changeTableEvent = async () => {
+      loading.value = true;
       const tableId = formData.value.table;
       if (tableId) {
         const table = await bitable.base.getTableById(tableId);
         const selection = await bitable.base.getSelection();
         const view = await table.getViewById(selection.viewId);
-        const recordList = await view.getVisibleRecordIdList(); // 获取有序record的id列表
-        if (endIndexValue > recordList.length - 1) {
-          alert("结束索引不能大于记录总数");
-          return [];
+        fieldMetaList.value = await view.getFieldMetaList(); // 获取所有字段元数据
+        for (let item of fieldMetaList.value) {
+          fieldMetaMap.value[item.id] = item;
         }
-        const NoPassData = []; // 未通过验证的数组数据
+        // 清空其他数据
+        noPassTableData.value = [];
         NoPassDataFields.value = [];
-        for (let i = startIndexValue; i <= endIndexValue; i++) {
-          const record = await table.getRecordById(recordList[i]);
+        formData.value.fields = [
+          {
+            fieldID: "",
+            checkTypeValue: "",
+          },
+        ];
+      }
+      loading.value = false;
+    };
 
-          let ErrCount = 0; // 单条记录验证错误次数
-          let ErrFields = []; // 单条记录验证错误的field字段id
-          // 判断单条记录是否通过所有验证
-          formData.value.fields.forEach((field) => {
-            let verify = checkTypeValueFn(record, field, fieldMetaMap.value);
-            if (!verify) {
-              ErrFields.push(field.fieldID);
-              ErrCount++;
-            }
-          });
-
-          if (ErrCount !== 0) {
-            NoPassData.push(record);
-            NoPassDataFields.value.push(ErrFields);
-          }
-          ErrCount = 0; // 重置数据
-        }
-        console.log(NoPassDataFields)
-        return NoPassData;
+    // 小窗选择记录
+    const selectRecords = async () => {
+      const { tableId, viewId } = await bitable.base.getSelection();
+      const recordIdList = await bitable.ui.selectRecordIdList(tableId, viewId);
+      const table = await bitable.base.getActiveTable();
+      for (const recordId of recordIdList) {
+        selectRecordList.value.push(await table.getRecordById(recordId)); // 获取选中的记录
       }
     };
-    /* 
-      1. 去除重复: 用单条记录进行所有验证, 每个验证不通过的记录+1, 最后记录不为0的才存入NoPassData
-      2. 高亮处理: 维护一个二维数组, 存入的二维度数组为每条NoPassData记录的不通过的列字段id
-      [
-        ["fieldID—1"],
-        ["fieldID—1", "fieldID—4"],     // 这里没有去重复列字段多验证类型的情况
-        ["fieldID—1"],
-      ]
-    */
 
-    // 点击查询选中table的所有记录
-    const checkAllRecords = async () => {
+    // 点击查询记录
+    const checkSelectRecords = async () => {
       loading.value = true;
       const recordList = await getAllRecordList();
+      if (recordList.length === 0) {
+        // 全局提示
+        await bitable.ui.showToast({
+          toastType: "error",
+          message: "请先选择需要检查的记录",
+        });
+        return;
+      }
+
       noPassTableData.value = [];
       for (let item of recordList) {
         let data = {};
@@ -150,13 +139,61 @@ export default {
     };
 
     // 删除检查属性
-    const delCheckProp = () => {
+    const delCheckProp = async () => {
       if (formData.value.fields.length === 1) {
-        alert("至少保留一个检查属性");
+        // 全局提示
+        await bitable.ui.showToast({
+          toastType: "error",
+          message: "至少保留一个检查属性",
+        });
         return;
       }
       formData.value.fields.pop();
     };
+
+    // ************************ 业务代码 ****************************
+    // 获取选中table的所有记录
+    const getAllRecordList = async () => {
+      if (selectRecordList.value.length == 0) {
+        return [];
+      }
+      const tableId = formData.value.table;
+      if (tableId) {
+        const NoPassData = []; // 未通过验证的数组数据
+        NoPassDataFields.value = [];
+        for (let i = 0; i < selectRecordList.value.length; i++) {
+          const record = selectRecordList.value[i];
+
+          let ErrCount = 0; // 单条记录验证错误次数
+          let ErrFields = []; // 单条记录验证错误的field字段id
+          // 判断单条记录是否通过所有验证
+          formData.value.fields.forEach((field) => {
+            let verify = checkTypeValueFn(record, field, fieldMetaMap.value);
+            if (!verify) {
+              ErrFields.push(field.fieldID);
+              ErrCount++;
+            }
+          });
+
+          if (ErrCount !== 0) {
+            NoPassData.push(record);
+            NoPassDataFields.value.push(ErrFields);
+          }
+          ErrCount = 0; // 重置数据
+        }
+        console.log(NoPassDataFields);
+        return NoPassData;
+      }
+    };
+    /* 
+      1. 去除重复: 用单条记录进行所有验证, 每个验证不通过的记录+1, 最后记录不为0的才存入NoPassData
+      2. 高亮处理: 维护一个二维数组, 存入的二维度数组为每条NoPassData记录的不通过的列字段id
+      [
+        ["fieldID—1"],
+        ["fieldID—1", "fieldID—4"],     // 这里没有去重复列字段多验证类型的情况
+        ["fieldID—1"],
+      ]
+    */
 
     // 错误单元格样式设置
     const cellStyle = ({ row, column, rowIndex, columnIndex }) => {
@@ -173,33 +210,6 @@ export default {
           }
         }
       }
-    };
-
-    // 改变table表
-    const changeTableEvent = async () => {
-      loading.value = true;
-      const tableId = formData.value.table;
-      if (tableId) {
-        const table = await bitable.base.getTableById(tableId);
-        const selection = await bitable.base.getSelection();
-        const view = await table.getViewById(selection.viewId);
-        fieldMetaList.value = await view.getFieldMetaList(); // 获取所有字段元数据
-        for (let item of fieldMetaList.value) {
-          fieldMetaMap.value[item.id] = item;
-        }
-        // 清空其他数据
-        noPassTableData.value = [];
-        NoPassDataFields.value = [];
-        formData.value.fields = [
-          {
-            fieldID: "",
-            checkTypeValue: "",
-          },
-        ];
-        formData.value.startIndex = 0;
-        formData.value.endIndex = 0;
-      }
-      loading.value = false;
     };
 
     // ************************ 类型方法 ****************************
@@ -228,19 +238,25 @@ export default {
       checkTypeList,
       loading,
       noPassTableData,
+      selectRecordList,
 
       cellStyle,
-      checkAllRecords,
+      checkSelectRecords,
       addCheckProp,
       delCheckProp,
       changeNumRange,
       changeTableEvent,
+
+      selectRecords,
     };
   },
 };
 </script>
 
 <template>
+  <!-- 标题介绍 -->
+  <HeaderIntro></HeaderIntro>
+
   <div class="form_container" v-loading="loading">
     <el-form ref="form" class="form" :model="formData" label-position="top">
       <el-form-item label="选择数据表" size="large">
@@ -264,7 +280,6 @@ export default {
         v-for="(field, fieldIndex) in formData.fields"
         :key="fieldIndex"
       >
-        <div class="top_line"></div>
         <el-form-item label="选择字段" size="large">
           <el-select
             v-model="field.fieldID"
@@ -280,18 +295,27 @@ export default {
           </el-select>
         </el-form-item>
         <el-form-item label="选择检查类型" size="large">
-          <el-select
-            v-model="field.checkTypeValue"
-            placeholder="请选择检查类型"
-            style="width: 100%"
-          >
-            <el-option
-              v-for="(type, index) in checkTypeList"
-              :key="index"
-              :label="type.label"
-              :value="type.value"
-            />
-          </el-select>
+          <div class="check_and_btn">
+            <el-select
+              v-model="field.checkTypeValue"
+              placeholder="请选择检查类型"
+              style="width: 100%"
+            >
+              <el-option
+                v-for="(type, index) in checkTypeList"
+                :key="index"
+                :label="type.label"
+                :value="type.value"
+              />
+            </el-select>
+            <!-- 新增或删除 -->
+            <div class="add_container">
+              <el-button type="success" @click="addCheckProp">添加</el-button>
+            </div>
+            <div class="del_container">
+              <el-button type="primary" @click="delCheckProp">删除</el-button>
+            </div>
+          </div>
           <!-- 显示对应检查类型的组件 -->
           <NumCharactersCheck
             v-show="field.checkTypeValue === 'numCharactersCheck'"
@@ -309,61 +333,37 @@ export default {
         </el-form-item>
       </div>
       <!-- 添加检查属性 -->
+      <el-form-item label="" size="large"> </el-form-item>
       <el-form-item label="" size="large">
-        <div class="add_container">
-          <div class="title">继续添加:</div>
-          <el-button type="success" @click="addCheckProp">添加</el-button>
-        </div>
-        <div class="del_container">
-          <div class="title">删除验证:</div>
-          <el-button type="primary" @click="delCheckProp">删除</el-button>
-        </div>
-      </el-form-item>
-      <el-form-item label="需要检查的列" size="large">
-        <!-- 两个input框, 一个输入起始索引, 一个输入结束索引 -->
-        <div class="index_container">
-          <div class="box">
-            <div class="title">起始索引:</div>
-            <el-input
-              v-model="formData.startIndex"
-              placeholder="请输入起始索引"
-              style="width: 60%"
-            />
-          </div>
-          <div class="box">
-            <div class="title">结束索引:</div>
-            <el-input
-              v-model="formData.endIndex"
-              placeholder="请输入结束索引"
-              style="width: 60%"
-            />
-          </div>
-        </div>
-      </el-form-item>
-      <el-button type="primary" plain size="large" @click="checkAllRecords"
-        >查询记录</el-button
-      >
-    </el-form>
-
-    <div class="no_pass_data">
-      <div class="title">验证不通过记录:</div>
-      <div class="table_data">
-        <el-table
-          :data="noPassTableData"
-          style="width: 100%"
-          height="350"
-          :cell-style="cellStyle"
+        <el-button type="success" plain size="large" @click="selectRecords"
+          >选择记录</el-button
         >
-          <el-table-column
-            v-for="(fieldHeader, fieldIndex) in fieldMetaList"
-            :key="fieldIndex"
-            :prop="fieldHeader.id"
-            :label="fieldHeader.name"
-            :fixed="fieldIndex === 0"
-            width="120"
-          />
-        </el-table>
-      </div>
+        <el-button type="primary" plain size="large" @click="checkSelectRecords"
+          >查询记录</el-button
+        >
+      </el-form-item>
+    </el-form>
+  </div>
+
+  <!-- 验证不通过数据展示 -->
+  <div class="no_pass_data">
+    <div class="title">验证不通过记录:</div>
+    <div class="table_data">
+      <el-table
+        :data="noPassTableData"
+        style="width: 100%"
+        height="350"
+        :cell-style="cellStyle"
+      >
+        <el-table-column
+          v-for="(fieldHeader, fieldIndex) in fieldMetaList"
+          :key="fieldIndex"
+          :prop="fieldHeader.id"
+          :label="fieldHeader.name"
+          :fixed="fieldIndex === 0"
+          width="120"
+        />
+      </el-table>
     </div>
   </div>
 </template>
@@ -373,62 +373,33 @@ export default {
   // 表单
   .form {
     &:deep(.el-form-item__label) {
-      font-size: 16px;
+      font-size: 14px;
       color: var(--el-text-color-primary);
-      margin-bottom: 0;
     }
     &:deep(.el-form-item__content) {
-      font-size: 16px;
+      font-size: 14px;
     }
-    .check_prop {
-      margin-top: 20px;
-      .top_line {
-        width: 100%;
-        height: 1px;
-        background-color: #aaa;
-        margin-bottom: 20px;
-      }
-    }
-    .add_container {
+
+    .check_and_btn {
       width: 100%;
       display: flex;
       align-items: center;
-      padding-top: 10px;
-      border-top: 1px solid #aaa;
-
-      .title {
-        margin-right: 10px;
+      justify-content: space-between;
+      .add_container {
+        margin-left: 15px;
       }
-    }
-    .del_container {
-      width: 100%;
-      display: flex;
-      align-items: center;
-      padding-top: 10px;
-
-      .title {
-        margin-right: 10px;
-      }
-    }
-    .index_container {
-      display: flex;
-      align-items: center;
-      .box {
-        width: 100%;
-        display: flex;
-        .title {
-          margin-right: 10px;
-        }
+      .del_container {
+        margin-left: 5px;
       }
     }
   }
+}
 
-  .no_pass_data {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    .title {
-      font-size: 16px;
-    }
+.no_pass_data {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  .title {
+    font-size: 14px;
   }
 }
 </style>
---
+++ src/components/HeaderIntro.vue
@@ -0,0 +1,46 @@
+<script>
+import { computed } from "vue";
+
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <!-- 介绍部分 -->
+  <div class="intro_container quote">
+    <div class="rule_tit">规则：</div>
+    <div class="include_fields">
+      类型详情：<a
+        href="https://lilithgames.feishu.cn/docx/JcAod2DOuorsR2xVhKYcANIgnWg"
+        target="_blank"
+        >链接地址</a
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+// 介绍部分
+.intro_container {
+  width: 100%;
+  .rule_tit {
+    font-size: 16px;
+    font-weight: bold;
+    padding-top: 10px;
+    margin-bottom: 12px;
+  }
+  .include_fields {
+    line-height: 22px;
+  }
+}
+.quote {
+  border-left: 3px solid #ccc;
+  padding-left: 10px;
+  font-size: 14px;
+  margin-bottom: 16px;
+  color: #606266;
+}
+</style>
